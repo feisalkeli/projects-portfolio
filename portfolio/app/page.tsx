014a5450fe8861a9330280730e6e368abfd5131b
Feisal Keli <105209149+feisalkeli@users.noreply.github.com>
2024-05-26 15:16:42 +0300

    💄 Create the Hero Section

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
-
+import Hero from "./components/Hero";
+import { FloatingNav as Navbar } from "./components/ui/floating-navbar";
+import { FaHome } from "react-icons/fa";
 export default function Home() {
   return (
-    <main>
-      <div className="text-3xl">Hello</div>
+    <main className="relative bg-black-100 h-[100vh] flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
+      <div className="max-w-7xl w-full">
+        <Navbar navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]} />
+        <Hero />
+      </div>
     </main>
   );
 }
